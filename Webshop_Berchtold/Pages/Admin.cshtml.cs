a4cf0a4ad260b5b0d06f6b4deec270fd5c68665e
BerchtoldLaurenz <laurenz.berchtold@htlwy.at>
2025-11-19 08:37:11 +0100

    Added Design for Admin Dashboard

--- Webshop_Berchtold/Pages/Admin.cshtml.cs
+++ Webshop_Berchtold/Pages/Admin.cshtml.cs
@@ -1,13 +1,35 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Webshop_Berchtold.Data;
+using Webshop_Berchtold.Models;
 
 namespace Webshop_Berchtold.Pages
 {
     [Authorize(Roles = "Admin")]
     public class AdminModel : PageModel
     {
-        public void OnGet()
+        private readonly ApplicationDbContext _context;
+
+        public AdminModel(ApplicationDbContext context)
         {
+            _context = context;
+        }
+
+        public List<Product> Products { get; set; } = new List<Product>();
+        public List<Category> Categories { get; set; } = new List<Category>();
+
+        public async Task OnGetAsync()
+        {
+            Products = await _context.Products
+                .Include(p => p.Kategorie)
+                .OrderBy(p => p.Id)
+                .ToListAsync();
+
+            Categories = await _context.Categories
+                .Include(c => c.Products)
+                .OrderBy(c => c.Id)
+                .ToListAsync();
         }
     }
 }
